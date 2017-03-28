6908e48461c01957429397dbdab53cb60f1886aa
tolik555 <tolik555.83@gmail.com>
2017-03-28 13:27:15 +0300

    11

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="ru">
+
 <head>
     <meta charset="UTF-8">
     <link href="styles/site.css" rel="stylesheet">
@@ -7,114 +8,107 @@
     <script src="scripts/site.js"></script>
     <title>Онлайн магазин</title>
 </head>
+
 <body>
-<header>
-    <div id="headerInside">
-        <div id="logo"></div>
-        <div id="companyName">Brand</div>
-        <div id="navWrap">
-            <a href="#">
+    <header>
+        <div id="headerInside">
+            <div id="logo"></div>
+            <div id="companyName">Brand</div>
+            <div id="navWrap">
+                <a href="/">
                 Главная
             </a>
-            <a href="#">
+                <a href="/?page=shop">
                 Магазин
             </a>
+            </div>
         </div>
-    </div>
-</header>
+    </header>
 
-<div id="content">
+    <div id="content">
 
-    <?php
+        <?php
     
-    function vardump($var) {
-      echo '<pre>';
-      var_dump($var);
-      echo '</pre>';
-    }
+//    function vardump($var) {
+//      echo '<pre>';
+//      var_dump($var);
+//      echo '</pre>';
+//    }
+//    
+//    $id = 1;
+//    $name = 'IPhone';
+//    $desc = 'Здесь будет описание IPhone';
+//    $img = '/images/goods/iphone.jpg';
+//    $price = '2000 $';
+//    
+//    echo $id . '<br>';
+//    echo $name . '<br>';
+//    echo $desc . '<br>';
+//    echo $img . '<br>';
+//    echo $price . '<br>';
+//    
+//    $product = [1, 'IPhone', 'Здесь будет описание IPhone', '/images/goods/iphone.jpg', '2000 $'];
+//    var_dump($product[1]);
+   ?><br>
+            <?
+//    $product_index = [
+//        'id'=>1,
+//        'name'=>'IPhone',
+//        'desc'=>'Здесь будет описание IPhone',
+//        'img'=>'/images/goods/iphone.jpg',
+//        'price'=>'2000 $'
+//    ];
+//    var_dump($product_index['name']);
+    ?><br>
+                <?
+//    $goods = [
+//        [
+//            'id'=> 1,
+//            'name'=> 'IPhone',
+//            'desc'=> 'Здесь будет описание IPhone',
+//            'img'=> '/images/goods/iphone.jpg',
+//            'price'=> '2000 $'
+//        ],
+//        [
+//            'id'=> 2,
+//            'name'=> 'HTC',
+//            'desc'=> 'Здесь будет описание HTC',
+//            'img'=> '/images/goods/htc.jpg',
+//            'price'=> '1000 $'
+//        ],
+//        [
+//            'id'=> 3,
+//            'name'=> 'Samsung',
+//            'desc'=> 'Здесь будет описание Samsung',
+//            'img'=> '/images/goods/samsung.jpg',
+//            'price'=> '400 $'
+//        ]
+//    ];
+//    
+//    var_dump($goods);
+//    
+//    foreach($goods as $key=>$good){
+//        echo 'Ключ масива:'.$key.'<br>';
+//        echo $good['id'].'<br>';
+//        echo $good['name'].'<br>';
+//        echo $good['desc'].'<br>';
+//        echo $good['img'].'<br>';
+//        echo $good['price'].'<br>';
+//        echo '<hr>';
+//    }
     
-    $id = 1;
-    $name = 'IPhone';
-    $desc = 'Здесь будет описание IPhone';
-    $img = '/images/goods/iphone.jpg';
-    $price = '2000 $';
-    
-    echo $id . '<br>';
-    echo $name . '<br>';
-    echo $desc . '<br>';
-    echo $img . '<br>';
-    echo $price . '<br>';
-    
-    $product = [1, 'IPhone', 'Здесь будет описание IPhone', '/images/goods/iphone.jpg', '2000 $'];
-    var_dump($product[1]);
-    ?><br><?
-    $product_index = [
-        'id'=>1,
-        'name'=>'IPhone',
-        'desc'=>'Здесь будет описание IPhone',
-        'img'=>'/images/goods/iphone.jpg',
-        'price'=>'2000 $'
-    ];
-    var_dump($product_index['name']);
-    ?><br><?
-    $goods = [
-        [
-            'id'=> 1,
-            'name'=> 'IPhone',
-            'desc'=> 'Здесь будет описание IPhone',
-            'img'=> '/images/goods/iphone.jpg',
-            'price'=> '2000 $'
-        ],
-        [
-            'id'=> 2,
-            'name'=> 'HTC',
-            'desc'=> 'Здесь будет описание HTC',
-            'img'=> '/images/goods/htc.jpg',
-            'price'=> '1000 $'
-        ],
-        [
-            'id'=> 3,
-            'name'=> 'Samsung',
-            'desc'=> 'Здесь будет описание Samsung',
-            'img'=> '/images/goods/samsung.jpg',
-            'price'=> '400 $'
-        ]
-    ];
-    
-    var_dump($goods);
-    
-    foreach($goods as $key=>$good){
-        echo 'Ключ масива:'.$key.'<br>';
-        echo $good['id'].'<br>';
-        echo $good['name'].'<br>';
-        echo $good['desc'].'<br>';
-        echo $good['img'].'<br>';
-        echo $good['price'].'<br>';
-        echo '<hr>';
+    $page = $_GET['page'];
+    if(!isset($page)){
+        require('tamplates/main.php');
+    } elseif($page=='shop'){
+        require('tamplates/shop.php');
+    } elseif($page == 'product') {
+        require('tamplates/openedProduct.php');
     }
     
     ?>
 
-    <!--<div id="promo">
-        <h1 id="promoText">
-            Здесь будет рекламный заголовок
-        </h1>
-    </div>
-
-    <div id="mainTextWrap">
-        <div id="mainText">
-            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-            Здесь будет текст для главной страницы. Здесь будет текст для главной страницы. Здесь будет текст для главной страницы.
-        </div>
-    </div>
-
+                    <!--
     <div style="width: 1100px; margin: 0 auto">
         <hr>
         <h2>
@@ -125,64 +119,6 @@
             <h1>
                 Каталог товаров
             </h1>
-            <div>
-                <div class="shopUnit">
-                    <img src="http://placehold.it/308x231" />
-
-                    <div class="shopUnitName">
-                        Название продукта
-                    </div>
-                    <div class="shopUnitShortDesc">
-                        Здесь будет немного текста описывающего продукт.
-                        Здесь будет немного текста описывающего продукт.
-                        Здесь будет немного текста описывающего продукт.
-                    </div>
-                    <div class="shopUnitPrice">
-                        Цена: 0000 $
-                    </div>
-                    <a href="#" class="shopUnitMore">
-                        Подробнее
-                    </a>
-                </div>
-
-                <div class="shopUnit">
-                    <img src="http://placehold.it/308x231" />
-
-                    <div class="shopUnitName">
-                        Название продукта
-                    </div>
-                    <div class="shopUnitShortDesc">
-                        Здесь будет немного текста описывающего продукт.
-                        Здесь будет немного текста описывающего продукт.
-                        Здесь будет немного текста описывающего продукт.
-                    </div>
-                    <div class="shopUnitPrice">
-                        Цена: 0000 $
-                    </div>
-                    <a href="#" class="shopUnitMore">
-                        Подробнее
-                    </a>
-                </div>
-
-                <div class="shopUnit">
-                    <img src="http://placehold.it/308x231" />
-
-                    <div class="shopUnitName">
-                        Название продукта
-                    </div>
-                    <div class="shopUnitShortDesc">
-                        Здесь будет немного текста описывающего продукт.
-                        Здесь будет немного текста описывающего продукт.
-                        Здесь будет немного текста описывающего продукт.
-                    </div>
-                    <div class="shopUnitPrice">
-                        Цена: 0000 $
-                    </div>
-                    <a href="#" class="shopUnitMore">
-                        Подробнее
-                    </a>
-                </div>
-            </div>
         </div>
 
         <div style="margin-bottom: 20px; float: left;">
@@ -190,61 +126,43 @@
             <h2>
                 Это будет отдельная страница с открытым товаром
             </h2>
-
-            <div id="openedProduct-img">
-                <img src="http://placehold.it/342x260">
-            </div>
-            <div id="openedProduct-content">
-                <h1 id="openedProduct-name">
-                    Название продукта
-                </h1>
-                <div id="openedProduct-desc">
-                    Здесь будет описание продукта. Здесь будет описание продукта. Здесь будет описание продукта.
-                    Здесь будет описание продукта. Здесь будет описание продукта. Здесь будет описание продукта.
-                    Здесь будет описание продукта. Здесь будет описание продукта. Здесь будет описание продукта.
-                </div>
-                <div id="openedProduct-price">
-                    Цена: 0000 $
-                </div>
             </div>
         </div>
 
 
     </div>-->
-</div>
-
-<footer>
-    <div id="footerInside">
-
-        <div id="contacts">
-            <div class="contactWrap">
-                <img src="images/envelope.svg" class="contactIcon">
-                info@brandshop.ru
-            </div>
-            <div class="contactWrap">
-                <img src="images/phone-call.svg" class="contactIcon">
-                8 800 555 00 00
-            </div>
-            <div class="contactWrap">
-                <img src="images/placeholder.svg" class="contactIcon">
-                Москва, пр-т Ленина, д. 1 офис 304
-            </div>
-        </div>
-
-        <div id="footerNav">
-            <a href="#">Главная</a>
-            <a href="#">Магазин</a>
-        </div>
-
-        <div id="social">
-            <img class="socialItem" src="images/vk-social-logotype.svg">
-            <img class="socialItem" src="images/google-plus-social-logotype.svg">
-            <img class="socialItem" src="images/facebook-logo.svg">
-        </div>
-
-        <div id="copyrights">&copy; Brand</div>
     </div>
-</footer>
+
+    <footer>
+        <div id="footerInside">
+
+            <div id="contacts">
+                <div class="contactWrap">
+                    <img src="images/envelope.svg" class="contactIcon"> info@brandshop.ru
+                </div>
+                <div class="contactWrap">
+                    <img src="images/phone-call.svg" class="contactIcon"> 8 800 555 00 00
+                </div>
+                <div class="contactWrap">
+                    <img src="images/placeholder.svg" class="contactIcon"> Москва, пр-т Ленина, д. 1 офис 304
+                </div>
+            </div>
+
+            <div id="footerNav">
+                <a href="/">Главная</a>
+                <a href="/?page=shop">Магазин</a>
+            </div>
+
+            <div id="social">
+                <img class="socialItem" src="images/vk-social-logotype.svg">
+                <img class="socialItem" src="images/google-plus-social-logotype.svg">
+                <img class="socialItem" src="images/facebook-logo.svg">
+            </div>
+
+            <div id="copyrights">&copy; Brand</div>
+        </div>
+    </footer>
 
 </body>
+
 </html>
---
+++ tamplates/shop.php
@@ -0,0 +1,20 @@
+<h1>
+    Каталог товаров
+</h1>
+
+<div class="shopUnit">
+    <img src="http://placehold.it/308x231" />
+
+    <div class="shopUnitName">
+        Название продукта
+    </div>
+    <div class="shopUnitShortDesc">
+        Здесь будет немного текста описывающего продукт. Здесь будет немного текста описывающего продукт. Здесь будет немного текста описывающего продукт.
+    </div>
+    <div class="shopUnitPrice">
+        Цена: 0000 $
+    </div>
+    <a href="#" class="shopUnitMore">
+        Подробнее
+    </a>
+</div>
